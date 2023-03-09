df657e670f239d06407e0a8079059038986b0c3c
Rodolphe Breton <rodolphe.breton@netgem.com>
2023-03-09 08:02:37 +0100

    Fixed lint errors

--- src/plugins/plugin.tooltip.js
+++ src/plugins/plugin.tooltip.js
@@ -7,8 +7,8 @@ export function update(...args) {
   const element = me.getActiveElements()[0];
 
   if (element) {
-    // 2023.01.04 
-    //const meta = me._chart.getDatasetMeta(element.datasetIndex);
+    // 2023.01.04
+    // const meta = me._chart.getDatasetMeta(element.datasetIndex);
     const meta = me.chart.getDatasetMeta(element.datasetIndex);
 
     me.$streaming = getAxisMap(me, transitionKeys, meta);
